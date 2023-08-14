1820590ddf678c64f353bdba93d4c870fc0854cb
inimomon <richiwirjaya@gmail.com>
2023-08-14 11:41:33 +0700

    add register form

---
+++ register.php
@@ -0,0 +1,22 @@
+<?php 
+include 'logic.php';
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $dataPemakai = register($_POST);
+}
+?>
+
+<?php 
+require_once 'navbar.php';
+?>
+
+<form action="" method="POST">
+    <div><input type="text" name="username"></div>
+    <div><input type="password" name="password"></div>
+
+    <div><button type="submit">Submit</button></div>
+</form>
+
+<?php 
+require_once 'footer.php';
+?>
